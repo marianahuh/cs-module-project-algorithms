6b2f3c5e58824758ed93a7ad125a3ff0a0755412
Mariana Huh <mhuh0176@gmail.com>
2020-06-11 17:06:40 -0700

    single number completed

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,8 +2,15 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+
+
+def single_number(arr):  # xor/bitwise operator
+    result = 0
+    # iterate through the arrary
+    for i in arr:
+        # xor will return 0 for same element and cancel out. otherwise retrun 1
+        result ^= i
+    return result
 
     pass
 
